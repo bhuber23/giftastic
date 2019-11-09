56248f81d6d1691ba3000477031d1bb13bc54901
Brittany Huber <brittanyleighhuber@gmail.com>
2019-11-08 19:28:44 -0700

    Added ajax function to pull in gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,13 +17,41 @@ for (var i=0; i < games.length; i++){
     $("#buttons").append(button);
 }
 
+//On click function for calling gifs
+$("button").on("click", function() {
+    var game = $(this).attr("data-game");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + 
+    game + "&api_key=326tZTOjHx2w5i7revWDUnIyufYTGjY7&limit=10";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+    .then(function(response) {
+        var results = response.data;
+        for (var i = 0; i < results.length; i++) {
+            if (results[i].rating !== "r") {
+                var gifDiv = $("<div id='results'>")
+                var rating = results[i].rating;
+                var p = $("<p>").text("Rating: " + rating);
+                var gameImage = $("<img>");
 
 
-//On click function for adding new gifs
+                gameImage.attr("src", results[i].images.fixed_height_still.url);
+                gameImage.attr("data-state", "still");
+                gameImage.attr("data-still", results[i].images.fixed_height_still.url);
+                gameImage.attr("data-animate", results[i].images.fixed_height.url);
+
+                gifDiv.append(p);
+                gifDiv.append(gameImage);
+
+                $("#gif-group").prepend(gifDiv);
+            }
+        }
+    })
+})
 
 
-
-//Add giphy API
+//Add new gifs with submit button
 
 
 
